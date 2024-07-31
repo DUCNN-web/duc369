9a4aebd27767237ddd7a4e0d7e7eb44230fe1458
duc <nguyenngocduc310@gmail.com>
2024-08-01 07:54:40 +0900

    baitap

---
+++ session08/lythuyet/lythuyet.js
@@ -0,0 +1,12 @@
+document.write("rikkei academy");
+// khai thác tuổi của 1 ng
+var age = 18;
+let fullName ="Vũ Hồng Vân";
+let checkEmailLogin;
+let $ = 1;
+let _ = 2;
+let isBoolean= 5>4;
+console.log(isBoolean);
+let checkIsNull= null;
+let checkIsNaN= "JS"-5;
+console.log(checkIsNaN);
